24b60a014e952bea227cc557e76378e90b381d48
Luana Kimley <73035581+luanakimley@users.noreply.github.com>
2021-11-24 11:55:01 +0000

    Added Booking getters and setters

--- src/main/java/org/example/Booking.java
+++ src/main/java/org/example/Booking.java
@@ -13,6 +13,76 @@ class Booking
 
     private double cost;  //Calculated at booking time
 
+    public int getBookingId()
+    {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId)
+    {
+        this.bookingId = bookingId;
+    }
+
+    public int getPassengerId()
+    {
+        return passengerId;
+    }
+
+    public void setPassengerId(int passengerId)
+    {
+        this.passengerId = passengerId;
+    }
+
+    public int getVehicleId()
+    {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId)
+    {
+        this.vehicleId = vehicleId;
+    }
+
+    public LocalDateTime getBookingDateTime()
+    {
+        return bookingDateTime;
+    }
+
+    public void setBookingDateTime(LocalDateTime bookingDateTime)
+    {
+        this.bookingDateTime = bookingDateTime;
+    }
+
+    public LocationGPS getStartLocation()
+    {
+        return startLocation;
+    }
+
+    public void setStartLocation(LocationGPS startLocation)
+    {
+        this.startLocation = startLocation;
+    }
+
+    public LocationGPS getEndLocation()
+    {
+        return endLocation;
+    }
+
+    public void setEndLocation(LocationGPS endLocation)
+    {
+        this.endLocation = endLocation;
+    }
+
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+
     //TODO - see specification
 
 }
